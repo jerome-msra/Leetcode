562b9dc7228e3ccd709e70871f134aa0dc63bfed
Jerome <jjlzyz000@gmail.com>
2016-06-07 16:00:46 -0400

    palindromepair and selfcrossing

---
+++ PalindromePairs.h
@@ -0,0 +1,13 @@
+//
+//  PalindromePairs.h
+//  LeetCode
+//
+//  Created by Jerome on 6/6/16.
+//  Copyright © 2016 Jerome. All rights reserved.
+//
+
+#ifndef PalindromePairs_h
+#define PalindromePairs_h
+
+
+#endif /* PalindromePairs_h */
---
+++ SelfCrossing.h
@@ -0,0 +1,13 @@
+//
+//  SelfCrossing.h
+//  LeetCode
+//
+//  Created by Jerome on 6/7/16.
+//  Copyright © 2016 Jerome. All rights reserved.
+//
+
+#ifndef SelfCrossing_h
+#define SelfCrossing_h
+
+
+#endif /* SelfCrossing_h */
