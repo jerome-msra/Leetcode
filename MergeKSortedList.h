4e8d4056294f5da05e824abb7746e572e747a699
Jerome <jjlzyz000@gmail.com>
2016-06-07 22:41:26 -0400

    merge k sorted list

---
+++ MergeKSortedList.h
@@ -0,0 +1,44 @@
+//
+//  MergeKSortedList.h
+//  LeetCode
+//
+//  Created by Jerome on 6/7/16.
+//  Copyright © 2016 Jerome. All rights reserved.
+//
+
+#ifndef MergeKSortedList_h
+#define MergeKSortedList_h
+
+// This is a min heap, but must use greater comparison
+struct mycompare {
+    bool operator()(ListNode * a, ListNode * b) {
+        return a->val > b->val;
+    }
+};
+
+ListNode* mergeKLists(vector<ListNode*>& lists) {
+    ListNode * dummy = new ListNode(0);
+    ListNode * prev = dummy;
+    for (auto it = lists.begin(); it!=lists.end();) {
+        ListNode * t = *it;
+        if (t!=nullptr) it++;
+        else {
+            it = lists.erase(it);
+        }
+    }
+    
+    priority_queue<ListNode*, vector<ListNode*>, mycompare> myqueue(lists.begin(), lists.end());
+    while(!myqueue.empty()) {
+        ListNode * next = myqueue.top();
+        prev->next = next;
+        prev = next;
+        myqueue.pop();
+        if (next->next!=nullptr) {
+            myqueue.push(next->next);
+        }
+    }
+    
+    return dummy->next;
+}
+
+#endif /* MergeKSortedList_h */
