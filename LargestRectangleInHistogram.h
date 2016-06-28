310d369e83a8831d91cb1ac15a93db3377fbb9d9
Jerome <jjlzyz000@gmail.com>
2016-06-28 18:17:00 -0400

    largest area in histogram

---
+++ LargestRectangleInHistogram.h
@@ -0,0 +1,35 @@
+//
+//  LargestRectangleInHistogram.h
+//  LeetCode
+//
+//  Created by Jerome on 6/28/16.
+//  Copyright © 2016 Jerome. All rights reserved.
+//
+
+#ifndef LargestRectangleInHistogram_h
+#define LargestRectangleInHistogram_h
+
+#include "DataStructure.h"
+
+using namespace std;
+
+int largestRectangleArea(vector<int>& heights) {
+    heights.push_back(0);
+    int n = heights.size();
+    stack<int> myIdxStack;
+    int left=0, right=0, height = 0;
+    int result = 0;
+    while(right < n) {
+        if (myIdxStack.empty() || heights[right]>heights[myIdxStack.top()]) myIdxStack.push(right++);
+        else {
+            height = heights[myIdxStack.top()];
+            myIdxStack.pop();
+            left = (myIdxStack.empty()) ? -1 : myIdxStack.top();
+            result = max(result, height * (right-left-1));
+        }
+    }
+    
+    return result;
+}
+
+#endif /* LargestRectangleInHistogram_h */
